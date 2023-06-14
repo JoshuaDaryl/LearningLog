e295429637bb2b6589f239691d0a877a3b1f60f1
Josh Halladey <135657835+JoshuaDaryl@users.noreply.github.com>
2023-06-14 20:32:48 +0100

    Practicing teachings from 'Python for everyone'

---
+++ Py4E/list.py
@@ -0,0 +1,16 @@
+print("SHOPPING LIST")
+
+list = list()
+
+for i in range(1,1000):
+    item = input("Please enter an item to add to your shopping list (type 'Quit' to finish list)\n")
+    item = item.capitalize()
+    if item != "Quit":
+        list.append(item)
+        continue
+        
+    else:
+        print(f"Here is yout shopping list:\n {list}")
+        quit()
+
+    
